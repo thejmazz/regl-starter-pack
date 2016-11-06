fc89f0abf81e3451dd6cd2ff6dc67acac9677eb4
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-05 22:16:32 -0400

    use raw loader for glsl shaders

--- src/index.js
+++ src/index.js
@@ -2,51 +2,31 @@ import reglConstructor from 'regl'
 
 const regl = reglConstructor()
 
+import frag from './shaders/frag.glsl'
+import vert from './shaders/vert.glsl'
+
 const drawTriangle = regl({
-  // Shaders in regl are just strings.  You can use glslify or whatever you want
-  // to define them.  No need to manually create shader objects.
-  frag: `
-    precision mediump float;
-    uniform vec4 color;
-    void main() {
-      gl_FragColor = color;
-    }`,
-
-  vert: `
-    precision mediump float;
-    attribute vec2 position;
-    void main() {
-      gl_Position = vec4(position, 0, 1);
-    }`,
-
-  // Here we define the vertex attributes for the above shader
+  frag,
+  vert,
   attributes: {
-    // regl.buffer creates a new array buffer object
     position: regl.buffer([
-      [-2, -2],   // no need to flatten nested arrays, regl automatically
-      [4, -2],    // unrolls them into a typedarray (default Float32)
+      [-2, -2],
+      [4, -2],
       [4,  4]
     ])
-    // regl automatically infers sane defaults for the vertex attribute pointers
   },
-
   uniforms: {
-    // This defines the color of the triangle to be a dynamic variable
     color: regl.prop('color')
   },
-
-  // This tells regl the number of vertices to draw in this command
   count: 3
 })
 
 regl.frame(({time}) => {
-  // clear contents of the drawing buffer
   regl.clear({
     color: [0, 0, 0, 0],
     depth: 1
   })
 
-  // draw a triangle using the command defined above
   drawTriangle({
     color: [
       Math.cos(time * 0.1),
