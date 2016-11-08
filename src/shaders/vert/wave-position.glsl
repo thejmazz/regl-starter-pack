e1ee0847ed551bf7925de5b6a1d9db20765a2f90
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-11-07 21:09:07 -0500

    normals?

--- src/shaders/vert/wave-position.glsl
+++ src/shaders/vert/wave-position.glsl
@@ -9,11 +9,16 @@ varying vec3 vnormal;
 
 void main () {
     vec3 p2 = position;
-    p2.z += sin(t + p2.y*p2.x);
+    p2.z += sin(t + p2.y);
 
-    vnormal = normal;
-    vnormal = normalize(p2);
-    /* vnormal.z = p2.z; */
+    float dx = 0.1; // some small number
+    float dy = (sin(t + p2.y + dx) - sin(t + p2.y - dx)) / 2.0;
+    // Now you have the two components of your tangent vector:
+    vec2 tangent = vec2(dx, dy);
+    // Which in 2d, you can rotate 90º to get a normal
+    vnormal = vec3(-tangent.y, tangent.x, 0);
+
+
     gl_Position = projection * view * model * vec4(p2, 1.0);
 }
 
